89de5219287d4f9cedc81a7ff15652e1f7174a73
BRAHMA SAI DEEPIKA <saideepikabrahma@gmail.com>
2026-07-14 14:19:14 +0530

    Implement task storage functions for localStorage

---
+++ modules/storage.js
@@ -0,0 +1,13 @@
+export function saveTasks(tasks) {
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify(tasks)
+  );
+}
+
+export function loadTasks() {
+  return (
+    JSON.parse(localStorage.getItem("tasks"))
+    || []
+  );
+}
